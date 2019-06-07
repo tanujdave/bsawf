cde14a2c687a8ccbfce1ecc145c14403a4632d6b
Tanuj Dave <tanuj.dave@printi.com.br>
2019-06-07 14:45:23 +0530

    save changes

--- snakeeyes/app.py
+++ snakeeyes/app.py
@@ -1,24 +1,23 @@
 from flask import Flask
+            
+from snakeeyes.blueprints.page import page
 
 
-def create_app():
+def create_app(settings_override=None):
     """
     Create a Flask application using the app factory pattern.
 
+    :param settings_override: Override settings
     :return: Flask app
     """
     app = Flask(__name__, instance_relative_config=True)
 
     app.config.from_object('config.settings')
-    app.config.from_pyfile('settings.py', silent=True)
+    app.config.from_pyfile('settings.py', silent=True)  
+                                
+    if settings_override:
+        app.config.update(settings_override)
 
-    @app.route('/')
-    def index():
-        """
-        Render a Hello World response.
-
-        :return: Flask response
-        """
-        return 'Hello World!!'
+    app.register_blueprint(page)
 
     return app
